fc7ba5d8234092b35a43577e4baa9e944b894c19
Napoleao1 <ernaninandoxd@gmail.com>
2024-12-01 20:22:23 -0300

    recebendo informações do usuário através do teclado

--- io-prompt.js
+++ io-prompt.js
@@ -6,18 +6,31 @@ const prompt = rL.createInterface({
 })
 
 const promptPromise = {
-    question: (pergunta) new Promise((resolve, reject) =>{
+    question: (pergunta) => new Promise((resolve, reject) =>{
         try {
             prompt.question((pergunta), (resposta) => resolve(resposta))
         } catch (error) {
           reject(error)
         }
     }),
-    close: prompt.close
+    close: () =>  prompt.close()
 }
 
-prompt.question('Qual seu número favorito? ', (resposta) => {
-    console.log(`O dobro do seu número favorito é: ${parseInt(resposta) *2}`)
-    prompt.close()
-})
+//prompt.question('Qual seu número favorito? ', (resposta) => {
+ //   console.log(`O dobro do seu número favorito é: ${parseInt(resposta) *2}`)
+  //  prompt.close()
+//})
 
+
+async function askUser () {
+
+  const numero = await promptPromise.question('Qual é o seu número favorito?: ')
+  console.log(`O dobro do seu número favorito é: ${parseInt(numero) * 2}`)
+
+  const cor = await promptPromise.question('Qual sua cor favorita?: ')
+  console.log(`sua cor favorita  é: ${(cor)}`)
+  promptPromise.close()
+
+}
+
+askUser()
