c86a9ee80baa6bb4c1c5e86290b330d95d713738
Napoleao1 <ernaninandoxd@gmail.com>
2024-12-01 20:12:40 -0300

    criando promptpromise

--- io-prompt.js
+++ io-prompt.js
@@ -5,6 +5,19 @@ const prompt = rL.createInterface({
     output: process.stdout
 })
 
+const promptPromise = {
+    question: (pergunta) new Promise((resolve, reject) =>{
+        try {
+            prompt.question((pergunta), (resposta) => resolve(resposta))
+        } catch (error) {
+          reject(error)
+        }
+    }),
+    close: prompt.close
+}
+
 prompt.question('Qual seu número favorito? ', (resposta) => {
     console.log(`O dobro do seu número favorito é: ${parseInt(resposta) *2}`)
+    prompt.close()
 })
+
