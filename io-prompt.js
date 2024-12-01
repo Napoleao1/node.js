c097deed61d177d9a674700b04736b6552bb4432
Napoleao1 <ernaninandoxd@gmail.com>
2024-12-01 20:29:33 -0300

    criando arquivo.js

--- io-prompt.js
+++ io-prompt.js
@@ -1,36 +1,28 @@
-const rL = require('readline')
+const rL = require('readline');
 
 const prompt = rL.createInterface({
-    input: process.stdin,                                             
+    input: process.stdin,
     output: process.stdout
-})
+});
 
 const promptPromise = {
-    question: (pergunta) => new Promise((resolve, reject) =>{
+    question: (pergunta) => new Promise((resolve, reject) => {
         try {
-            prompt.question((pergunta), (resposta) => resolve(resposta))
+            prompt.question(pergunta, (resposta) => resolve(resposta));
         } catch (error) {
-          reject(error)
+            reject(error);
         }
     }),
-    close: () =>  prompt.close()
+    close: () => prompt.close()
+};
+
+async function askUser() {
+    const numero = await promptPromise.question('Qual é o seu número favorito?: ');
+    console.log(`O dobro do seu número favorito é: ${parseInt(numero) * 2}`);
+
+    const cor = await promptPromise.question('Qual sua cor favorita?: ');
+    console.log(`Sua cor favorita é: ${cor}`);
+    promptPromise.close();
 }
 
-//prompt.question('Qual seu número favorito? ', (resposta) => {
- //   console.log(`O dobro do seu número favorito é: ${parseInt(resposta) *2}`)
-  //  prompt.close()
-//})
-
-
-async function askUser () {
-
-  const numero = await promptPromise.question('Qual é o seu número favorito?: ')
-  console.log(`O dobro do seu número favorito é: ${parseInt(numero) * 2}`)
-
-  const cor = await promptPromise.question('Qual sua cor favorita?: ')
-  console.log(`sua cor favorita  é: ${(cor)}`)
-  promptPromise.close()
-
-}
-
-askUser()
+askUser();
